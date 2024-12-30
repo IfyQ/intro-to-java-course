da7929ca3306ab0b393db3893e820cbdce10934e
Ify_isife <ifyisife@yahoo.com>
2024-12-30 15:31:03 +0000

    added missing exercises

--- exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
+++ exercises/flow-control/src/main/java/com/cbfacademy/FlowControlExercises.java
@@ -7,19 +7,44 @@ import java.util.Map;
 public class FlowControlExercises {
 
     public List<String> fizzBuzz(List<Integer> numbers) {
-        // TODO - Implement this method such that
+        // Implement this method such that
+      
         //  it creates a list where for each element of the input list ${numbers}
+        List<String> fizzBuzzList = new ArrayList<>();
+           //using for loop to check
+        for (int i = 0; i < numbers.size()); i++ {
+            interger number = numbers.get(i);{
         //  - if the ${element} is divisible by 3, it adds “Fizz” to the list
+       if  (number % 3 == 0) && (number % 5 == 0);{
+        fizzBuzzList.add("“FizzBuzz”");
+        else if (number % 3 == 0);{
+            fizzBuzzList.add("Fizz");
+        }
         //  - if the ${element} is divisible by 5, it adds “Buzz” to the list
+         else if {
+            (number % 5 == 0);
+                fizzBuzzList.add("Buzz");
+        }
         //  - if the ${element} is divisible by both 3 and 5, it adds “FizzBuzz” to the list
+          else if {
+           
+          }
         //  - it adds the element to the list in any other case
-        //  - it returns the constructed list
-        throw new RuntimeException("Not implemented");
-    }
+        else {
 
+            fizzBuzzList.add(number.toString);//convert interger to string
+        }
+           
+        //  - it returns the constructed list
+       {return fizzBuzzList;
+   
+        //throw new RuntimeException("Not implemented");
+    }
+    }
     public String whichMonth(Integer number) {
-        // TODO - Implement this method such that
+        // Implement this method such that
         //  - it returns the month corresponding to the input ${number}
+
         //  - if the ${number} is invalid, the method should return "Invalid month number"
         throw new RuntimeException("Not implemented");
     }
@@ -45,3 +70,5 @@ public class FlowControlExercises {
         return "Flow Control Exercises";
     }
 }
+        }
+    }
--- exercises/object-oriented-programming/exercises/algorithms/search/src/main/java/com/cbfacademy/search/App.java
+++
@@ -1,7 +0,0 @@
-package com.cbfacademy.search;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
